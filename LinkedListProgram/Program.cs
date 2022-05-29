bb7eb0e3020c99c0219684f5c48d996cdb23b08f
Vivek kumar <viveksinghssm496@gmail.com>
2022-05-29 11:33:04 +0530

    Added to main

---
+++ LinkedListProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LinkedListProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t\tWelcome to Linked List program");
+        }
+    }
+}
